613f303d2adcda78a222a4ddeeb57e383de8001b
0zbyy <ulviisfandiyarli0@gmail.com>
2023-12-22 22:00:47 +0400

    teze tasklarin

--- samir_task.py
+++ samir_task.py
@@ -1,10 +1,24 @@
 val = {
    "name" : "Samir", "age" : 19,
    "name" : "Ulvi", 'age' : 14,
-     
+
 }
 
 
 
 
 # bu dictionaryni sort ele
+
+
+
+
+
+
+# def ile fire ve dusmen funksiyasi yaz her fire istifade edende dusmenin cani azalsin
+
+
+
+
+
+
+# lambda ile factorial yaz
