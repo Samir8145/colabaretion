2ecd4c6d3820255b80600290abd2c8c49adff1ac
Samir8145 <mirzoevsamir814@gmail.com>
2023-12-24 11:53:29 +0400

    Yeni tesk

---
+++ ulvi_task.py
@@ -0,0 +1,16 @@
+# Factorial yarad
+
+
+def factorial(f):
+    if f == 0:
+        return 1
+    else:
+        return f * factorial(f-1)
+    
+
+
+print(factorial(5))
+
+
+# s = ['Men Python dilini orgenirem'] Bu listden boşluğu sil
+
