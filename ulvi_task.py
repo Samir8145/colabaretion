4d7e86e87a9a44d97fc2ccb2b67692de6cd82d99
0zbyy <ulviisfandiyarli0@gmail.com>
2023-12-24 13:53:20 +0400

    tasks completed

--- ulvi_task.py
+++ ulvi_task.py
@@ -1,16 +1,19 @@
 # Factorial yarad
 
 
-def factorial(f):
-    if f == 0:
-        return 1
-    else:
-        return f * factorial(f-1)
+# def factorial(f):
+#     if f == 0:
+#         return 1
+#     else:
+#         return f * factorial(f-1)
     
 
 
-print(factorial(5))
+# print(factorial(5))
 
 
 # s = ['Men Python dilini orgenirem'] Bu listden boşluğu sil
 
+text =  ['Men Python dilini orgenirem']
+text_without_spaces = text[0].replace(" ", "")
+print(text_without_spaces)
